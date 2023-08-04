bfe306636cb5a83487f2fe6763f0013a9e89fba1
sleep-sheep26 <133972968+sleep-sheep26@users.noreply.github.com>
2023-08-04 09:40:42 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,112 @@
+// pages/index/index.js
+var app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navH: 0,
+    rotationList:[
+      '../../assets/images/rotation/man.jpg',
+      '../../assets/images/rotation/student.jpg',
+      '../../assets/images/rotation/test1.jpg',
+      '../../assets/images/rotation/test2.jpg',
+      '../../assets/images/rotation/test3.jpg',
+      
+    ],
+    swiperCurrent: 0,
+
+    sortList:[
+      {
+        icon: "../../assets/images/sort/news.png",
+        sortid: 1,
+        text:"最新发布"
+      },{
+        icon: "../../assets/images/sort/second-hand.png",
+        sortid: 2,
+        text:"问题求助"
+      },{
+        icon: "../../assets/images/sort/love-mood.png",
+        sortid: 3,
+        text:"经验分享"
+      },{
+        icon: "../../assets/images/sort/question-ask.png",
+        sortid: 4,
+        text:"专业知识"
+      },
+    ],
+    testImage:[
+      'https://fishei.cn/static/pic/404.jpg',
+      'https://fishei.cn/static/pic/500.jpg',
+      'https://fishei.cn/static/pic/404.jpg',
+      'https://fishei.cn/static/pic/500.jpg',
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      navH: app.globalData.navHeight
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  //轮播图改变事件
+  swiperChange: function (e) {
+    if (e.detail.source === 'touch'){
+      this.setData({
+        swiperCurrent: e.detail.current
+      })
+    }
+  },
+})
